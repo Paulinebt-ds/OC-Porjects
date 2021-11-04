11197d46a524f7859256134d06dfff474a7e171e
Paulinebt-ds <85350007+Paulinebt-ds@users.noreply.github.com>
2021-11-04 12:27:12 +0100

    Prédiction avec séparation de la pipeline

--- app.py
+++ app.py
@@ -1,83 +1,68 @@
-from flask import Flask, jsonify, request
+import json
+
+from flask import Flask, request
 import pandas as pd
 from sklearn.preprocessing import PolynomialFeatures
-from joblib import dump, load
-import csv
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.impute import SimpleImputer
 
+import lightgbm as lgb
+from joblib import load
+import pickle
+import numpy as np
 app = Flask(__name__)
-
-
-@app.route('/form', methods=['GET', 'POST'])
-def get_row():
-    if request.method == 'POST':
-        row = request.form.get('row')
-        return row
-
-    # otherwise handle the GET request
-    return '''
-               <form method="POST">
-                   <div><label>Row: <input type="text" name="row"></label></div>
-                   <input type="submit" value="Submit">
-               </form>'''
-
-# Chemin des fichiers
-path = "C:/Users/pbliv/Documents/Data Science/P7"
-#Ligne du client que l'on souhaite charger
-## Ouverture du fichier
-f = open("./app_train.csv", 'r')
-## Lecture  du fichier
-r = csv.reader(f, delimiter=",", quoting=csv.QUOTE_NONNUMERIC)
-liste = list(r)
-i = get_row()
-data_client = liste[i]
-data_client = pd.DataFrame(data_client)
-# Fermeture du fichier
-f.close()
-
-# Dataframe pour les variables polynomiales
-poly_features = data_client[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3', 'DAYS_BIRTH']]
-
-# Imputation des valeurs manquantes par la moyenne
-imputer = load("./imputer_poly.joblib")
-poly_features = imputer.fit_transform(poly_features)
-
-# Création des polynômes avec un degré spécifique
-poly_transformer = PolynomialFeatures(degree=3)
-
-# Entraînement sur les variables polynomiales
-poly_transformer.fit(poly_features)
-# Transformation des variables
-poly_features = poly_transformer.transform(poly_features)
-
-# Concaténation des variables polynamiales avec l'ensemble d'entraînement
-poly_features['SK_ID_CURR'] = data_client['SK_ID_CURR']
-app_train_poly = data_client.merge(poly_features, on='SK_ID_CURR', how='left')
-
-# Variables métier
-data_domain = app_train_poly.copy()
-
-data_domain["CREDIT_INCOME_PERCENT"] = data_domain['AMT_CREDIT'] / data_domain['AMT_INCOME_TOTAL']
-data_domain["ANNUITY_INCOME_PERCENT"] = data_domain['AMT_ANNUITY'] / data_domain['AMT_INCOME_TOTAL']
-data_domain["CREDIT_TERM"] = data_domain['AMT_ANNUITY'] / data_domain['AMT_CREDIT']
-data_domain["DAYS_EMPLOYED_PERCENT"] = data_domain['DAYS_EMPLOYED'] / data_domain['DAYS_BIRTH']
 
 @app.route('/predict', methods = ['POST'])
 def predict():
+    if request.method == 'POST':
+        print(request)
+        row = request.get_json()
+        print(row)
+        row = json.loads(row)
+        print(row)
+    row = {k: [v] for k, v in row.items()}
+    print(row)
+    data_client = pd.DataFrame.from_dict(row)
+    cols = data_client.columns
+
+    # imputer = load("C:/Users/pbliv/Documents/Data Science/P7/imputer.joblib")
+    # data_client = imputer.transform(data_client)
+    # data_client = pd.DataFrame(data_client, columns=cols)
+
+    print(data_client.head())
     # calcul prédiction défaut et probabilité de défaut
+    payload = data_client
+    print("Payload, head :", payload.head())
 
     # Modèle sauvegardé via joblib
     my_model = load("C:/Users/pbliv/Documents/Data Science/P7/mymodel.joblib")
-    payload = data_domain
-    pred = my_model.predict(payload)
-    req_data = {"row": i,
-                "id": data_client.loc[:, 1],
+
+    # Modèle sauvegardé par LGBM
+    # Load the Model back from file
+    LGBM_model = lgb.Booster(model_file="C:/Users/pbliv/Documents/Data Science/P7/model.bin")
+
+    scaler = load("C:/Users/pbliv/Documents/Data Science/P7/scaler.joblib")
+    scaler.clip = False
+    print(scaler)
+
+    imputer = load("C:/Users/pbliv/Documents/Data Science/P7/imputer.joblib")
+    print(imputer)
+    print(LGBM_model)
+
+    payload = imputer.transform(payload)
+
+    payload = scaler.transform(payload)
+    pred = LGBM_model.predict(payload)
+    print(pred)
+    req_data = {"row": 2,
+                "id": data_client.iloc[:, 1],
                 "prediction": pred}
-    id_client = data_client.loc[:, 1]
+    id_client = data_client.iloc[:, 1]
     print('Nouvelle Prédiction : \n', req_data)
     return '''
                       <h1>Ligne du client choisi: {}</h1>
                       <h1>ID du client : {}</h1>
-                      <h1>Prédiction du client : {}</h1>'''.format(i, id_client, pred)
+                      <h1>Prédiction du client : {}</h1>'''.format(2, id_client, pred)
     #return jsonify(req_data)
 
 
