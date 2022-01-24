7fbcb1f3780b478de92fb264da839588dee9e29d
Paulinebt-ds <85350007+Paulinebt-ds@users.noreply.github.com>
2022-01-24 16:52:31 +0100

    Modification suite aux données API

--- Dashboard/ressources/components/functions.py
+++ Dashboard/ressources/components/functions.py
@@ -1,3 +1,8 @@
+import dash
+from dash import dcc
+from dash import html
+import dash_bootstrap_components as dbc
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -18,9 +23,9 @@ COLOR_BR = ['#EF553B', '#00CC96']
 
 def get_lime_explainer(data, X):
 
-  cat_feat_ix = [i for i, c in enumerate(data.drop(['SK_ID_CURR', 'TARGET'], axis=1).columns) if pd.api.types.is_categorical_dtype(data.drop(['SK_ID_CURR', 'TARGET'], axis=1)[c])]
-  feat_names = list(data.drop(['SK_ID_CURR', 'TARGET'], axis=1).columns)
-  class_names = list(data["TARGET"].unique())
+  cat_feat_ix = [i for i, c in enumerate(data.columns) if pd.api.types.is_categorical_dtype(data[c])]
+  feat_names = list(data.columns)
+  class_names = [0, 1]
   lime_explainer = LimeTabularExplainer(np.array(X),
                                       feature_names=feat_names,
                                       class_names=class_names,
@@ -30,8 +35,210 @@ def get_lime_explainer(data, X):
   return lime_explainer
 
 def lime_explain(explainer, data, predict_method, num_features):
-  explanation = explainer.explain_instance(data, predict_method, num_features=num_features)
+  explanation = explainer.explain_instance(data, predict_method, num_features=num_features, num_samples=100)
   return explanation
+
+
+corporate_colors = {
+    'dark-blue-grey' : 'rgb(62, 64, 76)',
+    'medium-blue-grey' : 'rgb(77, 79, 91)',
+    'superdark-green' : 'rgb(41, 56, 55)',
+    'dark-green' : 'rgb(57, 81, 85)',
+    'medium-green' : 'rgb(93, 113, 120)',
+    'light-green' : 'rgb(186, 218, 212)',
+    'pink-red' : 'rgb(255, 101, 131)',
+    'dark-pink-red' : 'rgb(247, 80, 99)',
+    'white' : 'rgb(251, 251, 252)',
+    'light-grey' : 'rgb(208, 206, 206)'
+}
+externalgraph_rowstyling = {
+    'margin-left' : '15px',
+    'margin-right' : '15px'
+}
+
+externalgraph_colstyling = {
+    'border-radius' : '10px',
+    'border-style' : 'solid',
+    'border-width' : '1px',
+    'border-color' : corporate_colors['superdark-green'],
+    'background-color' : corporate_colors['superdark-green'],
+    'box-shadow' : '0px 0px 17px 0px rgba(186, 218, 212, .5)',
+    'padding-top' : '10px'
+}
+
+navbarcurrentpage = {
+    'text-decoration': 'underline',
+    'color': 'white',
+    'text-decoration-color': corporate_colors['pink-red'],
+    'text-shadow': '0px 0px 1px rgb(251, 251, 252)',
+    'background-color': corporate_colors['dark-green']
+    }
+
+filterdiv_borderstyling = {
+    'border-radius' : '0px 0px 10px 10px',
+    'border-style' : 'solid',
+    'border-width' : '1px',
+    'border-color' : corporate_colors['light-green'],
+    'background-color' : corporate_colors['light-green'],
+    'box-shadow' : '2px 5px 5px 1px rgba(255, 101, 131, .5)'
+    }
+recapdiv = {
+    'border-radius' : '10px',
+    'border-style' : 'solid',
+    'border-width' : '1px',
+    'border-color' : 'rgb(251, 251, 252, 0.1)',
+    'margin-left' : '15px',
+    'margin-right' : '15px',
+    'margin-top' : '15px',
+    'margin-bottom' : '15px',
+    'padding-top' : '5px',
+    'padding-bottom' : '5px',
+    'background-color' : 'rgb(251, 251, 252, 0.1)'
+    }
+
+#####################
+# Nav bar
+def get_navbar(p = 'Global'):
+
+    navbar_global = html.Div([
+
+        html.Div([], className = 'col-3'),
+
+        html.Div([
+            dcc.Link(
+                html.H4(children = 'Global',
+                        style = navbarcurrentpage),
+                href='/apps/global'
+                )
+        ],
+        className='col-2'),
+
+        html.Div([
+            dcc.Link(
+                html.H4(children = 'Client',
+                        style = navbarcurrentpage),
+                href='/apps/Client'
+                )
+        ],
+        className='col-2'),
+
+        html.Div([
+            dcc.Link(
+                html.H4(children = 'Bivariée',
+                        style=navbarcurrentpage),
+                href='/apps/bivariate'
+                )
+        ],
+        className='col-2'),
+
+        html.Div([], className = 'col-3')
+
+    ],
+    className = 'row',
+    style = {'background-color' : corporate_colors['dark-green'],
+            'box-shadow': '2px 5px 5px 1px rgba(255, 101, 131, .5)'}
+    )
+
+    navbar_client = html.Div([
+
+        html.Div([], className = 'col-3'),
+
+        html.Div([
+            dcc.Link(
+                html.H4(children = 'Global',
+                        style=navbarcurrentpage),
+                href='/apps/global'
+                )
+        ],
+        className='col-2'),
+
+        html.Div([
+            dcc.Link(
+                html.H4(children = 'Client',
+                        style = navbarcurrentpage),
+                href='/apps/Client'
+                )
+        ],
+        className='col-2'),
+
+        html.Div([
+            dcc.Link(
+                html.H4(children = 'Bivariée',
+                        style=navbarcurrentpage),
+                href='/apps/bivariate'
+                )
+        ],
+        className='col-2'),
+
+        html.Div([], className = 'col-3',
+                 style=navbarcurrentpage)
+
+    ],
+    className = 'row',
+    style = {'background-color' : corporate_colors['dark-green'],
+            'box-shadow': '2px 5px 5px 1px rgba(255, 101, 131, .5)'}
+    )
+
+    navbar_bivariate = html.Div([
+
+        html.Div([], className = 'col-3'),
+
+        html.Div([
+            dcc.Link(
+                html.H4(children = 'Global',
+                        style=navbarcurrentpage),
+                href='/apps/global'
+                )
+        ],
+        className='col-2'),
+
+        html.Div([
+            dcc.Link(
+                html.H4(children = 'Client',
+                        style=navbarcurrentpage),
+                href='/apps/client'
+                )
+        ],
+        className='col-2'),
+
+        html.Div([
+            dcc.Link(
+                html.H4(children = 'Bivariée',
+                        style = navbarcurrentpage),
+                href='/apps/bivariate'
+                )
+        ],
+        className='col-2'),
+
+        html.Div([], className = 'col-3')
+
+    ],
+    className = 'row',
+    style = {'background-color' : corporate_colors['dark-green'],
+            'box-shadow': '2px 5px 5px 1px rgba(255, 101, 131, .5)'}
+    )
+
+    if p == 'global':
+        return navbar_global
+    elif p == 'client':
+        return navbar_client
+    else:
+        return navbar_bivariate
+
+# Empty row
+
+def get_emptyrow(h='45px'):
+    """This returns an empty row of a defined height"""
+
+    emptyrow = html.Div([
+        html.Div([
+            html.Br()
+        ], className = 'col-12')
+    ],
+    className = 'row',
+    style = {'height' : h})
+
+    return emptyrow
 
 def histogram(df, x='str', legend=True, client=None):
     '''client = [df_test, input_client] '''
